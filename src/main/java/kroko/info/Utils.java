838ebc3d065fc7848ee40d28467c3399bd98b26c
Krokodyl <3762526+Krokodyl@users.noreply.github.com>
2024-10-20 14:28:35 +0200

    comments

--- src/main/java/kroko/info/Utils.java
+++ src/main/java/kroko/info/Utils.java
@@ -9,20 +9,31 @@ import java.nio.file.NoSuchFileException;
 
 public class Utils {
 
+    /**
+     * Hexadecimal representation of a byte, with leading zeros (2 digits)
+     */
     public static String h2(byte b) {
         return String.format("%2s", Integer.toHexString(b & 0xFF)).replace(" ","0").toUpperCase();
     }
-    
+
+    /**
+     * Hexadecimal representation of an integer , with leading zeros (5 digits)
+     */
     public static String h5(int b) {
         return String.format("%5s", Integer.toHexString(b & 0xFFFFFF)).replace(" ","0").toUpperCase();
     }
 
+    /**
+     * Binary representation of a byte, with leading zeros (8 digits)
+     */
     public static String b8(byte b) {
         return String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(" ","0");
     }
 
+    /**
+     * Extract the header from the rom data based on the rom type
+     */
     public static int[] getSfcHeader(byte[] data, RomType type) {
-        
         int[] header = new int[type.getHeaderLength()];
         for (int i=0;i<header.length;i++) {
             header[i] = data[type.getOffsetHeader()+i];
@@ -62,14 +73,23 @@ public class Utils {
         return (byte) 0xC0;
     }
 
+    /**
+     * Converts the BS header day byte into regular number 1..31
+     */
     public static int getDay(byte b) {
         return ((b & 0xFF) >> 3);
     }
 
+    /**
+     * Converts the BS header month byte into regular number 1..12
+     */
     public static int getMonth(byte b) {
         return ((b & 0xFF) >> 4);
     }
 
+    /**
+     * Returns the BS header block allocation byte based on the given size (in bytes)
+     */
     public static byte getBlockFlag(int romSize) {
         int blockCount = romSize/0x20000;
         if (blockCount*0x20000<romSize) blockCount++;
@@ -80,6 +100,9 @@ public class Utils {
         return res;
     }
 
+    /**
+     * Returns the number of blocks (1..8) based on the given BS header block allocation byte
+     */
     public static int getBlockCount(byte b) {
         int bit = 8;
         int count = 0;
@@ -93,22 +116,34 @@ public class Utils {
         return count;
     }
 
+    /**
+     * Writes the byte array (@bytes) into the byte array (@target) at (@offset)
+     */
     public static void writeBytes(int[] target, byte[] bytes, int offset) {
         for (byte b : bytes) {
             target[offset++] = b;
         }
     }
 
+    /**
+     * Writes the byte (@b) into the byte array (@target) at (@offset)
+     */
     public static void writeByte(int[] target, byte b, int offset) {
         target[offset] = b;
     }
-    
+
+    /**
+     * Reads a 2-byte word (little endian)
+     */
     public static int readWord(byte[] data, int offset) {
         int word = (data[offset+1] & 0xFF)*0x100;
         word += (data[offset] & 0xFF);
         return word;
     }
-    
+
+    /**
+     * Reads a binary file as a byte array
+     */
     public static byte[] readRom(String inputRom) {
         byte[] data = new byte[0];
         try {
